a467467010ae39ce99498a96468e8e4af26d5133
Trambak Yesekar <trambakyesekar@gmail.com>
2024-03-21 22:30:31 +0530

    test case added

---
+++ src/test/java/com/example/TaskManagement/TaskManagementApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.TaskManagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
