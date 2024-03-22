93c5241f29ec32fd520dffefb17baf4acb7b922c
dilipyes <97657686+dilipyes@users.noreply.github.com>
2024-03-22 12:15:02 +0530

    test case removed

--- src/test/java/com/example/TaskManagement/TaskManagementApplicationTest.java
+++ src/test/java/com/example/TaskManagement/TaskManagementApplicationTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class TaskManagementApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 }
